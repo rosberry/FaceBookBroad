89576b5465c0efdf6d4f0e63fa4498c6df7e1ad8
nik <nikolay.tynin@rosberry.com>
2019-10-31 15:10:50 +0600

    add facebookbroad workspace

---
+++ FaceBookBroadExample/FaceBookBroadExample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  FaceBookBroadExample
+//
+//  Created by Nick Tyunin on 09/08/2019.
+//  Copyright © 2019 Rosberry. All rights reserved.
+//
+
+import UIKit
+import ButterBroad
+
+final class ViewController: UIViewController {
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func logEvengtDidTapped(_ sender: Any) {
+        if let text = textField.text, text.isEmpty == false {
+            Butter.common.logEvent(with: text)
+            textField.text = ""
+        }
+    }
+}
+
